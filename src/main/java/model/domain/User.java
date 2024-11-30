526ff78d9c7415cb9f408f3a8e6cd63d6f5b1504
류연경 <ykr.dev@gmail.com>
2024-12-01 01:03:02 +0900

    feat: User controller 추가 및 User 도메인 수정 - LoginController 추가 - LogoutController 추가 - RegisterUserController 추가 - UserSessionUtils 추가 - User.java: birthDate를 Date 대신 String으로 받을 수 있도록 생성자 수정

--- src/main/java/model/domain/User.java
+++ src/main/java/model/domain/User.java
@@ -19,9 +19,19 @@ public class User {
         this.birthDate = birthDate;
         this.isMorningPerson = isMorningPerson;
     }
-
+    
+    // birthDate를 Date 타입 말고 String으로도 받을 수 있게 함
+    public User(String userId, String password, String username, String email, String birthDateString, boolean isMorningPerson) {
+        this.userId = userId;
+        this.password = password;
+        this.username = username;
+        this.email = email;
+        this.birthDate = java.sql.Date.valueOf(birthDateString); // yyyy-MM-dd 형식의 String --> java.sql.Date로 변환 
+        this.isMorningPerson = isMorningPerson;
+    }
+    
     // 기본 생성자
-    public User() {}
+    public User() { };
 
     // Getters and Setters
     public String getUserId() {
