c29cfb96cb506e9d95bafd7b7c79a9466ba3167c
류연경 <ykr.dev@gmail.com>
2024-12-18 01:40:52 +0900

    feat: TaskMapper.java 파일 추가 및 UserMapper.java 변경 - UserMapper.java에 포함된 코드 분리하여 TaskMapper.java 파일로 추가 #24

---
+++ src/main/java/repository/mybatis/mapper/TaskMapper.java
@@ -0,0 +1,20 @@
+package repository.mybatis.mapper;
+
+import java.util.List;
+
+import model.domain.Task;
+
+public interface TaskMapper {
+    
+    int addTask(Task task);
+    
+    int updateTask(Task task);
+    
+    int deleteTask(int taskNo);
+    
+    Task getTaskById(int taskNo);
+    
+    List<Task> getAllTasks();
+    
+    List<Task> getTasksByUserId(int userNo);
+}
