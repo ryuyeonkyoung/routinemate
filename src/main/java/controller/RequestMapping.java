f7099ade7a4cee544af36952b97565669e0eab94
류연경 <ykr.dev@gmail.com>
2024-12-20 12:18:31 +0900

    feat : RequestMapping.java 마이페이지 매핑 #4

--- src/main/java/controller/RequestMapping.java
+++ src/main/java/controller/RequestMapping.java
@@ -17,20 +17,29 @@ public class RequestMapping {
     private Map<String, Controller> mappings = new HashMap<String, Controller>();
 
     public void initMapping() {
-//    	// 각 uri에 대응되는 controller 객체를 생성 및 저장
+    	    // 각 uri에 대응되는 controller 객체를 생성 및 저장
         mappings.put("/", new ForwardController("index.jsp"));
         mappings.put("/user/login/form", new ForwardController("/user/loginForm.jsp"));
         mappings.put("/user/login", new LoginController());
         mappings.put("/user/logout", new LogoutController());
 //        mappings.put("/user/list", new ListUserController());
 //        mappings.put("/user/view", new ViewUserController());
+        
+        //메인페이지
         mappings.put("/user/mainpage", new ForwardController("/user/mainpage.jsp")); 
-        mappings.put("/user/mypage", new ForwardController("/user/mypage.jsp")); 
-//        // 회원 가입 폼 요청과 가입 요청 처리 병합 (폼에 커뮤니티 선택 메뉴 추가를 위함)
+        
+        // 마이페이지
+        mappings.put("/user/mypage", new TaskController()); 
+        mappings.put("/user/mypage/create", new TaskController()); 
+        mappings.put("/user/mypage/update", new TaskController()); 
+        mappings.put("/user/mypage/delete", new TaskController()); 
+        
+//       // 회원 가입 폼 요청과 가입 요청 처리 병합 (폼에 커뮤니티 선택 메뉴 추가를 위함)
         mappings.put("/user/register/form", new ForwardController("/user/registerForm.jsp"));
-////      mappings.put("/user/register", new RegisterUserController());
         mappings.put("/user/register", new RegisterUserController());
-//
+
+        // 
+        
 //        // 사용자 정보 수정 폼 요청과 수정 요청 처리 병합
 ////      mappings.put("/user/update/form", new UpdateUserFormController());
 ////      mappings.put("/user/update", new UpdateUserController());        
